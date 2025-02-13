6b8c3dfcf2dfde3443d384d80c050120eed93cd6
rahim8050 <rahimranxx8050@gmail.com>
2025-02-13 14:32:39 +0000

    ins

--- resources/views/home/update.blade.php
+++ resources/views/home/update.blade.php
@@ -1,32 +1,163 @@
-<x-card>
-<form method="POST" class="max-w-lg mx-auto p-6 bg-white rounded-lg shadow-md">
-    @csrf
-    @method('PUT')
+<x-layout>
 
-    <div class="mb-4">
-        <label for="title" class="block text-gray-700 text-sm font-bold mb-2">Job Title:</label>
-        <input type="text" id="title" name="title" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-    </div>
+    <x-card class=" max-w-lg mx-auto mt-24"
+    >
+        <header class="text-center">
+            <h2 class="text-2xl font-bold uppercase mb-1">
+                Edit Gig
+            </h2>
+            {{-- <p class="mb-4">Edit: {{$listing->title}}</p> --}}
+        </header>
 
-    <div class="mb-4">
-        <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Job Description:</label>
-        <textarea id="description" name="description" rows="4" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
-    </div>
+        <form method="POST" action="" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="mb-6">
+                <label
+                    for="company"
+                    class="inline-block text-lg mb-2"
+                    >Company Name</label
+                >
+                <input
+                    type="text"
+                    class="border border-gray-200 rounded p-2 w-full"
+                    name="company" 
+                />
+                @error('company')
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
+            </div>
 
-    <div class="mb-4">
-        <label for="location" class="block text-gray-700 text-sm font-bold mb-2">Location:</label>
-        <input type="text" id="location" name="location" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-    </div>
+            <div class="mb-6">
+                <label for="title" class="inline-block text-lg mb-2"
+                    >Job Title</label
+                >
+                <input
+                    type="text"
+                    class="border border-gray-200 rounded p-2 w-full"
+                    name="title"
+                    placeholder="Example: Senior Laravel Developer"
+                   
+                />
+                @error('title')
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
+            </div>
 
-    <div class="mb-4">
-        <label for="salary" class="block text-gray-700 text-sm font-bold mb-2">Salary:</label>
-        <input type="text" id="salary" name="salary"  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-    </div>
+            <div class="mb-6">
+                <label
+                    for="location"
+                    class="inline-block text-lg mb-2"
+                    >Job Location</label
+                >
+                <input
+                    type="text"
+                    class="border border-gray-200 rounded p-2 w-full"
+                    name="location"
+                    placeholder="Example: Remote, Boston MA, etc"
+                   
+                />
+                @error('location')
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
+            </div>
 
-    <div class="flex items-center justify-between">
-        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-            Update Job
-        </button>
-    </div>
-</form>
-</x-card>
+            <div class="mb-6">
+                <label for="email" class="inline-block text-lg mb-2"
+                    >Contact Email</label
+                >
+                <input
+                    type="text"
+                    class="border border-gray-200 rounded p-2 w-full"
+                    name="email"
+                   
+                />
+                @error('email')
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
+            </div>
+
+            <div class="mb-6">
+                <label
+                    for="website"
+                    class="inline-block text-lg mb-2"
+                >
+                    Website/Application URL
+                </label>
+                <input
+                    type="text"
+                    class="border border-gray-200 rounded p-2 w-full"
+                    name="website"
+                   
+                />
+                @error('website')
+        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+        @enderror
+            </div>
+
+            <div class="mb-6">
+                <label for="tags" class="inline-block text-lg mb-2">
+                    Tags (Comma Separated)
+                </label>
+                <input
+                    type="text"
+                    class="border border-gray-200 rounded p-2 w-full"
+                    name="tags"
+                    placeholder="Example: Laravel, Backend, Postgres, etc"
+                   
+                />
+                @error('tags')
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
+            </div>
+
+            <div class="mb-6">
+                <label for="logo" class="inline-block text-lg mb-2">
+                    Company Logo
+                </label>
+                <input
+                    type="file"
+                    class="border border-gray-200 rounded p-2 w-full"
+                    name="logo"
+                  
+                />
+                {{-- <img
+                            class="w-48 mr-6 mb-6"
+                             src="{{ $listing->logo ? asset('storage/'.$listing->logo) : asset('/images/no-image.png') }}"
+            alt=""
+                        /> --}}
+
+                
+                @error('logo')
+        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+        @enderror
+            </div>
+
+            <div class="mb-6">
+                <label
+                    for="description"
+                    class="inline-block text-lg mb-2"
+                >
+                    Job Description
+                </label>
+                <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10"
+                placeholder="Include tasks, requirements, salary, etc"></textarea>
+      
+
+        @error('description')
+        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+        @enderror
+            </div>
+
+            <div class="mb-6">
+                <button
+                    class="bg-laravel text-white rounded py-2 px-4 hover:bg-black"
+                >
+                    Update Gig
+                </button>
+
+                <a href="/" class="text-black ml-4"> Back </a>
+            </div>
+        </form>
+    </x-card>
+</x-layout>
