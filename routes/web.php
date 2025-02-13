dbadaa99aa555b3077e037b4a1a8a75e26449148
rahim8050 <rahimranxx8050@gmail.com>
2025-02-13 14:59:42 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\Jobs;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+        return view('welcome');
+    })->name('dashboard');
+
 Route::get('/login', [Jobs::class, 'login'])->name('login');
 Route::get('/table', [Jobs::class, 'table'])->name('table');
 Route::get('/register',[Jobs::class, 'register'])->name('register');
