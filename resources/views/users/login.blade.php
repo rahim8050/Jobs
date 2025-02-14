1e71e13ce6300a5621927ecc6dac7e70e4b8b188
rahim8050 <rahimranxx8050@gmail.com>
2025-02-14 17:48:08 +0000

    login

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -20,7 +20,7 @@
                                     <form method="post" class="user">
                                         @csrf
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
+                                            <input type="email" name="email" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
                                                 placeholder="Enter Email Address...">
                                                 @error('email')
@@ -28,7 +28,7 @@
                                                 @enderror
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
+                                            <input type="password" name="password" class="form-control form-control-user"
                                                 id="exampleInputPassword" placeholder="Password">
                                                 @error('password')
                                                     <p class="text-danger">enter a valid password</p>
