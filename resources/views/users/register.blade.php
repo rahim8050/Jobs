cc76835c9e52b2510021f8bfe48fbedd68177858
rahim8050 <rahimranxx8050@gmail.com>
2025-02-15 11:11:17 +0000

    ins

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -14,59 +14,110 @@
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                                 </div>
-                                <form method="POST" action="/users" class="user">
-                                    @csrf
-                                    <div class="form-group row">
-                                        <div class="col-sm-6 mb-3 mb-sm-0">
-                                            <input type="text" name="name" class="form-control form-control-user" id="exampleFirstName"
-                                                placeholder="First Name">
-                                                @error('name')
-                                                <p class="text-red-500 text xs mt-1">{{$message}}</p> 
-                                                @enderror
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <input type="text" name="lastname" class="form-control form-control-user" id="exampleLastName"
-                                                placeholder="Last Name">
-                                                @error('lastname')
-                                                <p class="text-red-500 text xs mt-1">{{$message}}</p>
-                                                @enderror
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail"
-                                            placeholder="Email Address">
-                                            @error('email')
-                                            <p class="text-red-500 text xs mt-1">{{$message}}</p>
+                               
+                                    <x-card class="p-10  max-w-lg mx-auto mt-24 ">
+                                
+                                    <header class="text-center">
+                                        <h2 class="text-2xl font-bold uppercase mb-1">
+                                            Register
+                                        </h2>
+                                        <p class="mb-4">Create an account to post gigs</p>
+                                    </header>
+                                
+                                    <form method="POST" action="/users">
+                                        @csrf
+                                        <div class="mb-6">
+                                            <label for="name" class="inline-block text-lg mb-2">
+                                                Name
+                                            </label>
+                                            <input
+                                                type="text"
+                                                class="border border-gray-200 rounded p-2 w-full"
+                                                name="name" value="{{old('name')}}"
+                                            />
+                                            @error('name')
+                                               <p class="text-red-500 text xs mt-1">{{$message}}</p> 
                                             @enderror
-                                    </div>
-                                    <div class="form-group row">
-                                        <div class="col-sm-6 mb-3 mb-sm-0">
-                                            <input type="password" name="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
-                                                @error('password')
-                                                <p class="text-red-500 text xs mt-1">{{$message}}</p>
-                                                @enderror
                                         </div>
-
-                                        <div class="col-sm-6">
-                                            <input type="password" name="password_confirmation" class="form-control form-control-user"
-                                                id="exampleRepeatPassword" placeholder="Password_confirmation">
-                                                @error('password_confirmation')
-                                                <p class="text-red-500 text xs mt-1">{{$message}}</p>
-                                                @enderror
+                                
+                                        <div class="mb-6">
+                                            <label for="email" class="inline-block text-lg mb-2"
+                                                >Email</label
+                                            >
+                                            <input
+                                                type="email"
+                                                class="border border-gray-200 rounded p-2 w-full"
+                                                name="email" value="{{old('email')}}"
+                                            />
+                                            @error('email')
+                                            <p class="text-red-500 text-xs mt-1">
+                                                Please enter a valid email
+                                            </p> 
+                                            @enderror
+                                           
                                         </div>
-                                    </div>
-                                    <a href="/users" class="btn btn-primary btn-user btn-block">
-                                        Register Account
-                                    </a>
-                                    <hr>
-                                    <a href="/users" class="btn btn-google btn-user btn-block">
-                                        <i class="fab fa-google fa-fw"></i> Register with Google
-                                    </a>
-                                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                        <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
-                                    </a>
-                                </form>
+                                
+                                        <div class="mb-6">
+                                            <label
+                                                for="password"
+                                                class="inline-block text-lg mb-2"
+                                            >
+                                                Password
+                                            </label>
+                                            <input
+                                                type="password"
+                                                class="border border-gray-200 rounded p-2 w-full"
+                                                name="password" value="{{old('password')}}"
+                                            />
+                                            @error('password')
+                                            <p class="text-red-500 text-xs mt-1">
+                                                Please enter a valid email
+                                            </p> 
+                                            @enderror
+                                        </div>
+                                
+                                        <div class="mb-6">
+                                            <label
+                                                for="password2"
+                                                class="inline-block text-lg mb-2"
+                                            >
+                                                Confirm Password
+                                            </label>
+                                            <input
+                                                type="password"
+                                                class="border border-gray-200 rounded p-2 w-full"
+                                                name="password_confirmation" value="{{old('password_confirmation')}}"
+                                            />
+                                            @error('password_confirmation')
+                                            <p class="text-red-500 text-xs mt-1">
+                                                Please enter a valid email
+                                            </p> 
+                                            @enderror
+                                        </div>
+                                
+                                        <div class="mb-6">
+                                            <button
+                                                type="submit"
+                                                class="bg-laravel text-white rounded py-2 px-4 hover:bg-black"
+                                            >
+                                                Sign Up
+                                            </button>
+                                        </div>
+                                
+                                        <div class="mt-8">
+                                            <p>
+                                                Already have an account?
+                                                <a href="login.html" class="text-laravel"
+                                                    >Login</a
+                                                >
+                                            </p>
+                                        </div>
+                                    </form>
+                                    </x-card>
+                                
+                               
+                                     
+                                    
                                 <hr>
                                 <div class="text-center">
                                     <a class="small" href="forgot-password.html">Forgot Password?</a>
