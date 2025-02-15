4a33c1d0f09c80479dc24209ab23f4b299ed9067
rahim8050 <rahimranxx8050@gmail.com>
2025-02-15 10:59:39 +0000

    ins

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -14,7 +14,8 @@
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                                 </div>
-                                <form class="user">
+                                <form method="POST" action="/users" class="user">
+                                    @csrf
                                     <div class="form-group row">
                                         <div class="col-sm-6 mb-3 mb-sm-0">
                                             <input type="text" name="name" class="form-control form-control-user" id="exampleFirstName"
@@ -55,11 +56,11 @@
                                                 @enderror
                                         </div>
                                     </div>
-                                    <a href="login.html" class="btn btn-primary btn-user btn-block">
+                                    <a href="/users" class="btn btn-primary btn-user btn-block">
                                         Register Account
                                     </a>
                                     <hr>
-                                    <a href="index.html" class="btn btn-google btn-user btn-block">
+                                    <a href="/users" class="btn btn-google btn-user btn-block">
                                         <i class="fab fa-google fa-fw"></i> Register with Google
                                     </a>
                                     <a href="index.html" class="btn btn-facebook btn-user btn-block">
