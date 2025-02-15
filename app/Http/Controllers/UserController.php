ce61a0b8e7bcd1e5464399660bb1d81271940a13
rahim8050 <rahimranxx8050@gmail.com>
2025-02-15 11:44:17 +0000

    ins

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,18 +13,18 @@ class UserController extends Controller
         return view('users.register');
     }
     
-    // public function store(Request $request)
-    // {
-    //     $formFields = $request->validate([
-    //         'name' => ['required','min:3'],
-    //         'email' => ['required', 'email',Rule::unique('users', 'email')],
-    //         'password' => ['required', 'min:8','confirmed']
-    //     ]);
-    //     $formFields['password'] = bcrypt($formFields['password']);
-    //     $user = User::create($formFields);
-    //     Auth::login($user);
-    //     return redirect('/')->with('message', 'User Created successfully and logged in');
-    // }
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => ['required','min:3'],
+            'email' => ['required', 'email',Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8','confirmed']
+        ]);
+        $formFields['password'] = bcrypt($formFields['password']);
+        $user = User::create($formFields);
+        Auth::login($user);
+        return redirect('/')->with('message', 'User Created successfully and logged in');
+    }
     
     public function login(){
         return view('users.login');
