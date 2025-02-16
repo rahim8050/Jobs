a2bb1b4d15e7039e4919a9b15f1aab861baf6958
rahim8050 <rahimranxx8050@gmail.com>
2025-02-16 15:21:01 +0000

    ins

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () {
     })->name('dashboard');
 
 Route::get('/login', [UserController::class, 'login'])->name('login');
+// Route::get('/works/{work}',[])
 Route::get('/table', [Jobs::class, 'table'])->name('table');
 Route::get('/register',[UserController::class, 'register'])->name('register');
 Route::get('/update',[Jobs::class, 'update'])->name('update');
