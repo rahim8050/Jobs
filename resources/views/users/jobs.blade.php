bab9240de8b9ea7325011b493e7ff7e19731c1b4
rahim8050 <rahimranxx8050@gmail.com>
2025-02-18 12:35:17 +0000

    inside

--- resources/views/users/jobs.blade.php
+++ resources/views/users/jobs.blade.php
@@ -3,8 +3,14 @@
  
      <x-card>   
         <div
-                class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
-            >
+        class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
+    >
+    
+            <img
+                class="hidden w-48 mr-6 md:block"
+                src="{{ url ('frontend/images/rahim.jpeg')}}"
+                alt=""
+            
     @unless (count($jobs)==0)
 
     @foreach ($jobs as $job )
