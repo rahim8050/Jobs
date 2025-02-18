6bbf2e0adbbb4156720ba62b44bfaf65a8be506d
rahim8050 <rahimranxx8050@gmail.com>
2025-02-18 11:05:03 +0000

    ins

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,25 +1,22 @@
-<div {{$attributes->merge(['class'=>'bg-gray-50 border border-gray-200 rounded p-6'])}}>
- 
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script>
-        tailwind.config = {
+
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+      tailwind.config = {
           theme: {
-            extend: {
-              colors: {
-                'custom-blue': '#1e40af',
-              }
-            }
+              extend: {
+                  colors: {
+                      laravel: "#ef3b2d",
+                  },
+              },
           },
-          plugins: [],
-        }
-      </script>
-      
-    {{ $slot }}
-      
-    <footer
+      };
+  </script>
+
+  {{$slot}}
+  <footer
 class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
 >
-<p class="ml-2">Copyright &copy; 2024, All Rights reserved</p>
+<p class="ml-2">Copyright &copy; 2025, All Rights reserved</p>
 
 <a
     href="/listings/create" 
@@ -27,4 +24,3 @@ class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg
     >Post Job</a
 >
 </footer>
-    </div>
--- resources/views/users/jobs.blade.php
+++ resources/views/users/jobs.blade.php
@@ -1,8 +1,8 @@
-<x-card>
+
     {{-- @include('partials.search') --}}
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-        
-     
+ 
+     <x-card>   
+ 
     @unless (count($jobs)==0)
 
     @foreach ($jobs as $job )
@@ -18,8 +18,10 @@
     @else
     <p class="text-info">no job found</p>
     @endunless
-</div>
-</x-card>
+     </x-card>
+
+
+
 
 
 
