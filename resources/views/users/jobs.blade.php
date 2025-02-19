8d762e2655ce3cd1b782f025ebdcd1567f62d038
rahim8050 <rahimranxx8050@gmail.com>
2025-02-19 05:58:14 +0000

    update

--- resources/views/users/jobs.blade.php
+++ resources/views/users/jobs.blade.php
@@ -5,11 +5,47 @@
     @unless (count($jobs) == 0)
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
             @foreach ($jobs as $job)
-                <a href="/jobs/{{$job['id']}}">
-                    <h2>{{$job['title']}}</h2>
-                </a>
-                <p>{{$job['tags']}}</p>
-                <p class="text-success font-mono 5rem">{{$job['description']}}</p>
+                 <!-- Item 1 -->
+                 {{-- <div class="bg-gray-50 border border-gray-200 rounded p-6"> --}}
+                    <div class="flex">
+                        <img
+                            class="hidden w-48 mr-6 md:block"
+                            src="images/acme.png"
+                            alt=""
+                        />
+                        <div>
+                            <h3 class="text-2xl">
+                                <a href="show.html">{{$job->title}}</a>
+                            </h3>
+                            <div class="text-xl font-bold mb-4">Acme Corp</div>
+                            <ul class="flex">
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    <a href="#">Laravel</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    <a href="#">API</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    <a href="#">Backend</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    <a href="#">Vue</a>
+                                </li>
+                            </ul>
+                            <div class="text-lg mt-4">
+                                <i class="fa-solid fa-location-dot"></i> Boston,
+                                MA
+                            </div>
+                        </div>
+                    {{-- </div> --}}
             @endforeach
         </div>
     @else
