e855648c7f0c2094c6d36b3b865e6621586219af
rahim8050 <rahimranxx8050@gmail.com>
2025-02-19 06:25:05 +0000

    update

--- resources/views/users/jobs.blade.php
+++ resources/views/users/jobs.blade.php
@@ -1,7 +1,7 @@
 @include('partials.hero')
 @include('partials.search')
 
-<x-card>
+{{-- <x-card> --}}
     @unless (count($jobs) == 0)
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
             @foreach ($jobs as $job)
@@ -51,7 +51,7 @@
     @else
         <p class="text-info">no job found</p>
     @endunless
-</x-card>
+{{-- </x-card> --}}
 
 
 
