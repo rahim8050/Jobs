fd9841f0b70eebe06b4eb162a25b56d61a75a2d7
rahim8050 <rahimranxx8050@gmail.com>
2025-02-20 12:45:15 +0000

    ins

--- resources/views/users/job.blade.php
+++ resources/views/users/job.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+@extends('display')
 @section('content')
 
        <h2>{{$job['title']}}</h2>
@@ -6,4 +6,5 @@
    
        <p class="text-success font-mono 5rem">{{$job['description']}}</p>
        <p> {{$job['tags']}}</p>
+       <img src="{{url('frontend/img/logo.png')}}" alt="">
        @endsection
--- resources/views/users/jobs.blade.php
+++ resources/views/users/jobs.blade.php
@@ -1,23 +1,71 @@
 {{-- @include('partials.hero')
 @include('partials.search') --}}
-@extends('layout')
+@extends('display')
 @section('content')
     @unless (count($jobs) == 0)
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
             @foreach ($jobs as $job)
-                 <!-- Item 1 -->
-                 <div class="bg-gray-50 border border-gray-200 rounded p-6">
+          
+                <!-- Item 4 -->
+                <div class="bg-gray-50 border border-gray-200 rounded p-6">
                     <div class="flex">
                         <img
                             class="hidden w-48 mr-6 md:block"
-                            src="images/acme.png"
+                            src="images/skynet.png"
                             alt=""
                         />
                         <div>
                             <h3 class="text-2xl">
-                                <a href="/jobs/{{$job->id}}"">{{$job->title}}</a>
+                                <a href="show.html">Backend Laravel Dev</a>
                             </h3>
-                            <div class="text-xl font-bold mb-4">Acme Corp</div>
+                            <div class="text-xl font-bold mb-4">
+                                Skynet Systems
+                            </div>
+                            <ul class="flex">
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    Laravel
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    API
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    Backend
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    Vue
+                                </li>
+                            </ul>
+                            <div class="text-lg mt-4">
+                                <i class="fa-solid fa-location-dot"></i>
+                                Daytona, FL
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Item 5 -->
+                <div class="bg-gray-50 border border-gray-200 rounded p-6">
+                    <div class="flex">
+                        <img
+                            class="hidden w-48 mr-6 md:block"
+                            src="images/wonka.png"
+                            alt=""
+                        />
+                        <div>
+                            <h3 class="text-2xl">
+                                <a href="show.html">Junior Developer</a>
+                            </h3>
+                            <div class="text-xl font-bold mb-4">
+                                Wonka Industries
+                            </div>
                             <ul class="flex">
                                 <li
                                     class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
@@ -41,24 +89,17 @@
                                 </li>
                             </ul>
                             <div class="text-lg mt-4">
-                                <i class="fa-solid fa-location-dot"></i> Boston,
-                                MA
+                                <i class="fa-solid fa-location-dot"></i> San
+                                Francisco, CA
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
             @endforeach
         </div>
+        
     @else
         <p class="text-info">no job found</p>
     @endunless
     @endsection
-
-
-
-
-
-
-
-
-
-
