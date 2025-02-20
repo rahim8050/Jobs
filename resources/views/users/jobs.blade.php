66af67d9612910b261f4f0a2c1b338a125152b65
rahim8050 <rahimranxx8050@gmail.com>
2025-02-20 14:46:16 +0000

    correctness

--- resources/views/users/jobs.blade.php
+++ resources/views/users/jobs.blade.php
@@ -9,7 +9,7 @@
                 <div class="flex">
                     <img
                         class="hidden w-48 mr-6 md:block"
-                        src="images/acme.png"
+                        src="{{('frontend/img/rahim.jpeg')}}"
                         alt=""
                     />
                     <div>
