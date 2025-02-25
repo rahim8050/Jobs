0f83a43b33b76c99a7749cf5c3eb40868aaa0df1
rahim8050 <rahimranxx8050@gmail.com>
2025-02-25 09:07:21 +0000

    ins

--- resources/views/components/listig-card.blade.php
+++ resources/views/components/listig-card.blade.php
@@ -1,5 +1,5 @@
 @props(['job'])
-
+<x-card>
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
@@ -11,30 +11,10 @@
                 <a href="/jobs/{{$job->id}}">{{$job->title}}</a>
             </h3>
             <div class="text-xl font-bold mb-4">{{$job->company}}</div>
-            <ul class="flex">
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Laravel</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">API</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Backend</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Vue</a>
-                </li>
-            </ul>
+            <x-jobs-tags :tagsCsv="$job->tags" />
             <div class="text-lg mt-4">
                 <i class="fa-solid fa-location-dot"></i> {{$job->location}}
             </div>
         </div>
     </div>
+</x-card>
--- resources/views/users/job.blade.php
+++ resources/views/users/job.blade.php
@@ -7,8 +7,8 @@
 ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 
-
-
+<div class="mx-4">
+<x-card class="p-10">
     <div
         class="flex flex-col items-center justify-center text-center"
     >
@@ -20,28 +20,7 @@
 
         <h3 class="text-2xl mb-2">{{$job->title}}</h3>
         <div class="text-xl font-bold mb-4">{{$job->company}}</div>
-        <ul class="flex">
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">Laravel</a>
-            </li>
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">API</a>
-            </li>
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">Backend</a>
-            </li>
-            <li
-                class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-            >
-                <a href="#">Vue</a>
-            </li>
-        </ul>
+        <x-jobs-tags :tagsCsv="$job->tags" />
         <div class="text-lg my-4">
             <i class="fa-solid fa-location-dot"></i> {{$job->location}}
         </div>
@@ -69,6 +48,6 @@
             </div>
         </div>
     </div>
-
+</x-card>
 </div>
 @endsection
