c1077e2ba5fbcb48e782ddb9a85c2bd035303eb6
rahim8050 <rahimranxx8050@gmail.com>
2025-02-26 12:45:22 +0000

    ins

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -6,7 +6,42 @@ use Illuminate\Http\Request;
 
 class WorkController extends Controller
 {
-    
+    public function index()
+    {
+        return view('work.index');
+    }
+    public function create()
+    {
+        return view('work.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+        return redirect()->route('work.index');
+    }
+    public function show($id)
+    {
+        return view('work.show');
+    }
+    public function edit($id)
+    {
+        return view('work.edit');
+    }
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+        return redirect()->route('work.index');
+    }
+    public function destroy($id)
+    {
+        return redirect()->route('work.index');
+    }
   
   
 }
